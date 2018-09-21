3a695846fa9f7c50cc5855e986c8a3c49d65a802
charbelsako <charbelsarkis82581@gmail.com>
2018-09-22 01:27:40 +0300

    added BrowserRouter. And register theme

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,26 @@ import "./App.css"
 import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
 import Landing from "./components/layout/Landing"
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
 
 //Routing
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
-      <Route>
+      <Router>
         <div className="App">
           <Navbar />
           <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
           <Footer />
         </div>
-      </Route>
+      </Router>
     )
   }
 }
