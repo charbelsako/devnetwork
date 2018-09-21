cb3300d0b7ab46c50990357774f424d1de3ecc7c
charbelsako <charbelsarkis82581@gmail.com>
2018-09-22 01:16:30 +0300

    added navbar and footer. And the basic landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react"
 import "./App.css"
 
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+import Landing from "./components/layout/Landing"
+
+//Routing
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>My react app</h1>
-      </div>
+      <Route>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <Footer />
+        </div>
+      </Route>
     )
   }
 }
