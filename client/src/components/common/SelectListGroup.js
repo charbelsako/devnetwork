102343afdca957868031ed956cbb065c464bae21
charbelsako <charbelsarkis82581@gmail.com>
2018-09-24 16:10:21 +0300

    The select list component

---
+++ client/src/components/common/SelectListGroup.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import classnames from 'classnames'
+import PropTypes from 'prop-types'
+
+const SelectListGroup = ({
+  name,
+  placeholder,
+  value,
+  error,
+  info,
+  onChange,
+  options,
+}) => {
+  return (
+    <div className="form-group">
+      <textarea
+        className={classnames('form-control form-control-lg', {
+          'is-invalid': error,
+        })}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+      />
+      {info && <small className="form-text text-muted">{info}</small>}
+      {error && <div className="invalid-feedback">{error}</div>}
+    </div>
+  )
+}
+
+SelectListGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  info: PropTypes.string,
+  error: PropTypes.string,
+  disabled: PropTypes.string,
+}
+
+export default SelectListGroup
