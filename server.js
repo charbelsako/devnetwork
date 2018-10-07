d8d3cf996ebb17c0c9643a33d0e19d96038a30d5
charbelsako <charbelsarkis82581@gmail.com>
2018-10-07 11:04:44 +0300

    adding some changes to fix hello screen

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config() // get you env variables
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
@@ -40,10 +41,11 @@ app.use('/api/posts', posts)
 
 // For heroku
 // Serve static assets if in production
-if (process.env.NODE_ENV == 'production') {
+if (process.env.NODE_ENV === 'production') {
+  console.log('production mode')
   app.use(express.static('client/build'))
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'client/', 'build/', 'index.html'))
   })
 }
 
