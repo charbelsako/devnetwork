ea84dc1234c6ce09960bca6f84b5650a8178739d
Charbel Sarkis <charbelsako@gmail.com>
2021-06-10 11:27:18 +0300

    fix: :bug: wrong github username produces error on profile route
    If a user submits a wrong username as their github username and that username is not found on github, it returns a not found message instead of an array so I had to check if the returned data is an array or not

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
 class ProfileGithub extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      clientId: '1bf4ed366056f3377501',
-      clientSecret: '47bee98e5c64288933a5e2a58c9121017d3db28e',
+      clientId: "1bf4ed366056f3377501",
+      clientSecret: "47bee98e5c64288933a5e2a58c9121017d3db28e",
       count: 5,
-      sort: 'created: asc',
+      sort: "created: asc",
       repos: [],
-    }
+    };
   }
 
   componentDidMount() {
-    const { username } = this.props
-    const { clientId, clientSecret, count, sort } = this.state
+    const { username } = this.props;
+    const { clientId, clientSecret, count, sort } = this.state;
 
     fetch(
       `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}$client_secret=${clientSecret}`
@@ -24,14 +24,19 @@ class ProfileGithub extends Component {
       .then(res => res.json())
       .then(data => {
         if (this.refs.myref) {
-          this.setState({ repos: data })
+          if (Array.isArray(data.length)) {
+            this.setState({ repos: data });
+          }
         }
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        this.setState({ repos: [] });
+        console.log(err);
+      });
   }
 
   render() {
-    const { repos } = this.state
+    const { repos } = this.state;
 
     const repoItems = repos.map(repo => (
       <div key={repo.id} className="card card-body mb-2">
@@ -45,19 +50,13 @@ class ProfileGithub extends Component {
             <p>{repo.description}</p>
           </div>
           <div className="col-md-6">
-            <span className="badge badge-info mr-1">
-              Stars: {repo.stargazers_count}
-            </span>
-            <span className="badge badge-secondary mr-1">
-              Watcher: {repo.watchers_count}
-            </span>
-            <span className="badge badge-success">
-              Forks: {repo.forks_count}
-            </span>
+            <span className="badge badge-info mr-1">Stars: {repo.stargazers_count}</span>
+            <span className="badge badge-secondary mr-1">Watcher: {repo.watchers_count}</span>
+            <span className="badge badge-success">Forks: {repo.forks_count}</span>
           </div>
         </div>
       </div>
-    ))
+    ));
 
     return (
       <div ref="myref">
@@ -65,12 +64,12 @@ class ProfileGithub extends Component {
         <h3 className="mb-4">Latest github repos</h3>
         {repoItems}
       </div>
-    )
+    );
   }
 }
 
 ProfileGithub.propTypes = {
   username: PropTypes.string.isRequired,
-}
+};
 
-export default ProfileGithub
+export default ProfileGithub;
