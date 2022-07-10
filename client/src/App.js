d421649db55aa3bd36d25a9de924000e85203790
Sharbel Sarkis <charbelsako@gmail.com>
2022-07-10 21:04:32 +0000

    Added a joint route for employers and students

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,60 @@
-import React, { Component } from "react";
-import "./App.css";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
-import { setCurrentUser, logoutUser } from "./actions/authActions";
-import { clearCurrentProfile } from "./actions/profileActions";
+import React, { Component } from "react"
+import "./App.css"
+import jwt_decode from "jwt-decode"
+import setAuthToken from "./utils/setAuthToken"
+import { setCurrentUser, logoutUser } from "./actions/authActions"
+import { clearCurrentProfile } from "./actions/profileActions"
 // Custom Components
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
-import Landing from "./components/layout/Landing";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Profiles from "./components/profiles/Profiles";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/create-profile/CreateProfile";
-import EditProfile from "./components/edit-profile/EditProfile";
-import AddExperience from "./components/add-credentials/AddExperience";
-import AddEducation from "./components/add-credentials/AddEducation";
-import Profile from "./components/profile/Profile";
-import Ads from "./components/ads/Ads";
-import MyAds from "./components/ads/MyAds";
-import CreateAd from "./components/ads/CreateAd";
-import Users from "./components/admin/Users";
-import Comments from "./components/instructor/Comments";
+import Navbar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+import Landing from "./components/layout/Landing"
+import Login from "./components/auth/Login"
+import Register from "./components/auth/Register"
+import Profiles from "./components/profiles/Profiles"
+import Dashboard from "./components/dashboard/Dashboard"
+import CreateProfile from "./components/create-profile/CreateProfile"
+import EditProfile from "./components/edit-profile/EditProfile"
+import AddExperience from "./components/add-credentials/AddExperience"
+import AddEducation from "./components/add-credentials/AddEducation"
+import Profile from "./components/profile/Profile"
+import Ads from "./components/ads/Ads"
+import MyAds from "./components/ads/MyAds"
+import CreateAd from "./components/ads/CreateAd"
+import Users from "./components/admin/Users"
+import Comments from "./components/instructor/Comments"
 // Redux
-import { Provider } from "react-redux";
+import { Provider } from "react-redux"
 // Redux store
-import store from "./store";
+import store from "./store"
 
 // Routing
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./components/common/PrivateRoute";
-import NotFound from "./components/not-found/NotFound";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
-import EmployerRoute from "./components/common/EmployerRoute";
-import AdminRoute from "./components/common/AdminRoute";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import PrivateRoute from "./components/common/PrivateRoute"
+import NotFound from "./components/not-found/NotFound"
+import Posts from "./components/posts/Posts"
+import Post from "./components/post/Post"
+import EmployerRoute from "./components/common/EmployerRoute"
+import AdminRoute from "./components/common/AdminRoute"
+import EmployerStudentRoute from "./components/common/EmployerStudentRoute"
 
 // Check for token
 if (localStorage.jwtToken) {
   // set auth token header
-  setAuthToken(localStorage.jwtToken);
+  setAuthToken(localStorage.jwtToken)
   // Decode token
-  const decoded = jwt_decode(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken)
   // Set user and isAuthentication
-  store.dispatch(setCurrentUser(decoded));
+  store.dispatch(setCurrentUser(decoded))
 
   // Check for expired token
-  const currentTime = Date.now() / 1000;
+  const currentTime = Date.now() / 1000
   if (decoded.exp < currentTime) {
-    store.dispatch(logoutUser());
+    store.dispatch(logoutUser())
     // Clear current profile
     // TODO
     // Redirect to login
-    store.dispatch(clearCurrentProfile());
-    window.location.href = "/login";
+    store.dispatch(clearCurrentProfile())
+    window.location.href = "/login"
   }
 }
 
@@ -71,14 +72,32 @@ class App extends Component {
               <Route exact path="/profiles" component={Profiles} />
               <Route exact path="/profile/:handle" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
-              <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-              <PrivateRoute exact path="/edit-profile" component={EditProfile} />
-              <PrivateRoute exact path="/add-experience" component={AddExperience} />
-              <PrivateRoute exact path="/add-education" component={AddEducation} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
+              />
               <PrivateRoute exact path="/feed" component={Posts} />
               <PrivateRoute exact path="/post/:id" component={Post} />
               <EmployerRoute exact path="/myads" component={MyAds} />
-              <EmployerRoute exact path="/ads" component={Ads} />
+              {/* <EmployerRoute exact path="/ads" component={Ads} />
+              <PrivateRoute exact path="/ads" component={Ads} /> */}
+              <EmployerStudentRoute exact path="/ads" component={Ads} />
               <EmployerRoute exact path="/create-ad" component={CreateAd} />
               <AdminRoute exact path="/admin/users" component={Users} />
               <Route exact path="/:handle/comments" component={Comments} />
@@ -88,8 +107,8 @@ class App extends Component {
           </div>
         </Router>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
